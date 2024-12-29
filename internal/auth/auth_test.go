bf2f7408509060cc44166d14e25c6b20b9eaa809
Vegard Seines <vegsei@gmail.com>
2024-12-29 22:54:17 +0100

    refactored tests packages

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,11 +1,9 @@
-package auth_test
+package auth
 
 import (
 	"net/http"
 	"reflect"
 	"testing"
-
-	"github.com/bootdotdev/learn-cicd-starter/internal/auth"
 )
 
 func TestGetAPIKey(t *testing.T) {
@@ -18,7 +16,7 @@ func TestGetAPIKey(t *testing.T) {
 		{"simple", http.Header{"Authorization": {"ApiKey somekey"}}, "somekey"},
 	}
 	for _, tc := range testCases {
-		result, err := auth.GetAPIKey(tc.input)
+		result, err := GetAPIKey(tc.input)
 		if err != nil {
 			t.Fatalf("%s case: GetAPIKey returned error: %s", tc.name, err.Error())
 		}
